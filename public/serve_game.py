d24bc1765c13dc996bd1e2c084bc2c7edffaff91
sloanefoxofficial <sloanefox.official@gmail.com>
2026-08-27 21:21:37 +1000

    Delete public directory

--- public/serve_game.py
+++
@@ -1,6 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import os
-
-os.chdir(Path(__file__).parent / 'public')
-ThreadingHTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler).serve_forever()
